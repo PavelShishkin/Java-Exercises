9bb4c1bfcd71cf85aebf94ef0583babc00e1db9e
Pavel <pavel.frolov.1968@gmail.com>
2016-10-28 21:13:15 +0800

    update code

--- Repetitions.java
+++ Repetitions.java
@@ -1,18 +1,19 @@
 /**
  * Created by Pavel on 28.10.16.
  */
-public class Repetitions
+public class Java
 {
     public static void main(String[] args)
     {
-        String[] str = {"num","lam","mip","tex","bal","bod","bal","tex","end"};
+        String s = "num lam mip tex bal bod bal tex end";
+        String[] str = s.split(" ");
         String str2 = "";
 
         for (int i = 0; i < str.length; i++)
         {
             for (int j = i + 1; j < str.length; j++)
             {
-                if(str[i] == str[j])
+                if(str[i].equals(str[j]))
                 {
                     str2 = str[i] + " " + str2;
                 }
